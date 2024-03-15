0394b54e64b785de4b75f3530f597fdbeab07a50
onebarloop <alexanderbayer@posteo.de>
2024-03-15 16:45:19 +0100

    refactoring: components and tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,13 +13,20 @@ export default {
         pop: 'pop 0.5s ease-in',
       },
       fontFamily: {
-        'west': ['"Rye"', 'serif']
+        'west': ['"Rye"', 'serif'],
+        'oswald': ['"Oswald", sans-serif']
       },
       colors: {
         'ww-green': '#4EC69B',
         'ww-green-100': '#9EEFD0',
+        'ww-black': '#2d2e2b',
+        'ww-white': '#E8F4E7',
         'egg': '#F9F1CB',
-        'red': '#DE6138'
+        'red': '#DE6138',
+        'orange': '#FC6736',
+        'dark-orange': '#C28E24',
+        'purple': '#9E88ED'
+      
       },
       boxShadow: {
         'test': '0px 0px 16px 6px #4EC69B'
