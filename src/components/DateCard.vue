398f3ffd52093f23d3e6373d8847246d0664bd15
onebarloop <alexanderbayer@posteo.de>
2024-03-16 12:28:21 +0100

    temporary fix ui-icons

--- src/components/DateCard.vue
+++ src/components/DateCard.vue
@@ -16,11 +16,26 @@ defineProps<{
       <div class="flex justify-end">
         <a class="group flex items-center gap-1 text-lg font-bold" href="#">
           <span class="">Zum Termin</span>
-          <img
+          <!-- <img
             class="w-5 translate-y-[1px] transition-transform group-hover:translate-x-1"
-            src="../assets/icons/arrow-right.svg"
+            src="ui/arrow-right.svg"
             alt="Arrow Icon"
-          />
+          /> -->
+          <svg
+            class="w-5 translate-y-[1px] transition-transform group-hover:translate-x-1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="5" y1="12" x2="19" y2="12"></line>
+            <polyline points="12 5 19 12 12 19"></polyline>
+          </svg>
         </a>
       </div>
     </div>
