881f66045ead9c33f5d27c0dd173c5ad7718ba18
onebarloop <alexanderbayer@posteo.de>
2024-03-16 15:50:58 +0100

    test new img-import

--- src/components/DateCard.vue
+++ src/components/DateCard.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+
+import arrow from '../assets/icons/arrow-right.svg'
+
 defineProps<{
   title: string;
   date: string;
 }>();
+
 </script>
 
 <template>
+
   <article class="rounded bg-ww-black p-2">
     <div class="rounded bg-ww-white p-4">
       <h3 class="mb-2 flex gap-2 text-xl font-bold">
@@ -16,12 +21,12 @@ defineProps<{
       <div class="flex justify-end">
         <a class="group flex items-center gap-1 text-lg font-bold" href="#">
           <span class="">Zum Termin</span>
-          <!-- <img
+          <img
             class="w-5 translate-y-[1px] transition-transform group-hover:translate-x-1"
-            src="ui/arrow-right.svg"
+            :src="arrow.src"
             alt="Arrow Icon"
-          /> -->
-          <svg
+          />
+          <!-- <svg
             class="w-5 translate-y-[1px] transition-transform group-hover:translate-x-1"
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -32,10 +37,10 @@ defineProps<{
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-          >
-            <line x1="5" y1="12" x2="19" y2="12"></line>
+          > -->
+            <!-- <line x1="5" y1="12" x2="19" y2="12"></line>
             <polyline points="12 5 19 12 12 19"></polyline>
-          </svg>
+          </svg> -->
         </a>
       </div>
     </div>
